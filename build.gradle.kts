d73513772e33bdcbf2e2c564e895d6e067366ac8
woojoo <woojooseo0918@gmail.com>
2023-07-11 21:22:19 +0900

    and etc.. updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.8.20"
     application
 }
 
@@ -17,19 +17,14 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.10")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.10")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.20-RC")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.20-RC")
 
-    implementation(dependencyNotation = "io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
-    implementation("io.github.monun:kommand-api:3.1.2")
-    implementation("io.github.monun:tap-api:4.9.2")
+    implementation(dependencyNotation = "io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+    implementation("io.github.monun:kommand-api:3.1.6")
     implementation("io.github.monun:heartbeat-coroutines:0.0.5")
-    implementation("io.github.monun:invfx-api:3.3.0")
-}
-
-tasks.test {
-    useJUnitPlatform()
+    implementation("io.github.monun:tap-api:4.9.6")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
 }
 
 tasks.withType<KotlinCompile> {
