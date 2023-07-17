60665f13910750218ddec1d4b38c3412901ad182
w0819 <dasansaebom.s230210@ggm.goe.go.kr>
2023-07-17 10:29:53 +0900

    library update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.20"
+    kotlin("jvm") version "1.8.22"
     application
 }
 
@@ -17,14 +17,14 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.20-RC")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.20-RC")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.22")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.22")
 
     implementation(dependencyNotation = "io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
     implementation("io.github.monun:kommand-api:3.1.6")
     implementation("io.github.monun:heartbeat-coroutines:0.0.5")
     implementation("io.github.monun:tap-api:4.9.6")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
 }
 
 tasks.withType<KotlinCompile> {
